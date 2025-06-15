29bde0c45abc6ac1489ef8fd7a876ab13a028b60
dissan97 <dissanahmed@gmail.com>
2025-06-15 09:07:02 +0000

    added phold

--- build/experiments.sh
+++ build/experiments.sh
@@ -1,14 +1,17 @@
 xX#!/bin/bash
 
-declare lookaheads=(0.1 0.5 0.8 1.0 2.0)
-declare alphas=(0.25 0.5 1.0 2.0 5.0)
+#declare lookaheads=(0.1 0.5 0.8 1.0 2.0)
+declare runs=3
+declare lookaheads=(0.5 0.8 1.0)
+#declare alphas=(0.25 0.5 1.0 2.0 5.0)
+declare alphas=(0.5 1.0 2.0)
 #declare objects=(512 1024 4096)
-declare objects=(1024 2048 4096)
+declare objects=(1024 4096)
 declare queue=("ORIGIN" "LPTF")
 declare numa=("0" "1")
 declare balanced=("0" "1")
 #declare models=("pcs" "highway")
-declare models=("pcs")
+declare models=("pcs" "phold")
 balance="balanced"
 BALANCE="-BALANCE"
 NUMA=""
@@ -20,43 +23,45 @@ mkdir -p ../bin/simulation/balanced
 mkdir -p ../bin/simulation/unbalanced
 mkdir -p ../bin/simulation/logs
 
+for run in $(seq 1 $runs); do
+  for model in "${models[@]}"; do
+    for lookahead in "${lookaheads[@]}"; do
+      for alpha in "${alphas[@]}"; do
+        for object in "${objects[@]}"; do
+          for q in "${queue[@]}"; do
+            if [[ "$q" == "ORIGIN" && "$alpha" != "1.0" ]]; then
+              continue
+            fi 
+            if [[ "$model" == "phold" && "$lookahead" != "1.0" && "$object" != "4096" ]]; then continue; fi
+            for n in "${numa[@]}"; do
+              for b in "${balanced[@]}"; do
+                if [[ $b -eq 0 ]]; then
+                  balance="balanced"
+                  BALANCE="-BALANCE"
+                else
+                  balance="unbalanced"
+                  BALANCE="-UNBALANCE"
+                fi
+                if [[ $n -eq 1 ]]; then
+                  NUMA="-NUMA"
+                else
+                  NUMA=""
+                fi
+                echo "$lines"
+                config="${model}-${q}${NUMA}${BALANCE}-APH_${alpha}-LAH_${lookahead}-OBJ_${object}"
+                echo "Running configuration: $config"
+                echo "$lines"
+                make "$model" QUEUE_IMPL="$q" NUMA="$n" ALPHA="$alpha" LOOKAHEAD="$lookahead" OBJECTS="$object" UNBALANCE="$b"
 
-for model in "${models[@]}"; do
-  for lookahead in "${lookaheads[@]}"; do
-    for alpha in "${alphas[@]}"; do
-      for object in "${objects[@]}"; do
-        for q in "${queue[@]}"; do
-          if [[ "$q" == "ORIGIN" && "$alpha" != "1.0" ]] ; then
-             continue
-          fi
-          for n in "${numa[@]}"; do
-            for b in "${balanced[@]}"; do
-              if [[ $b -eq 0 ]]; then
-                balance="balanced"
-                BALANCE="-BALANCE"
-              else
-                balance="unbalanced"
-                BALANCE="-UNBALANCE"
-              fi
-              if [[ $n -eq 1 ]]; then
-                NUMA="-NUMA"
-              else
-                NUMA=""
-              fi
-              echo "$lines"
-              config="${model}-${q}${NUMA}${BALANCE}-APH_${alpha}-LAH_${lookahead}-OBJ_${object}"
-              echo "Running configuration: $config"
-              echo "$lines"
-              make "$model" QUEUE_IMPL="$q" NUMA="$n" ALPHA="$alpha" LOOKAHEAD="$lookahead" OBJECTS="$object" UNBALANCE="$b"
-              
-              bin_path="../bin/simulation/${balance}/PARSIR-simulator-${config}"
-              mv ../bin/PARSIR-simulator-${model}-${q}${NUMA}${BALANCE} ${bin_path}
-              echo "$lines"
-              echo "Simulation for $config"
-              ${bin_path} | grep 'Barrier measures\|throughput' > ../bin/simulation/logs/${config}.log
-              echo "$lines"
-              echo "Log file created at: ../bin/simulation/logs/${config}.log"
-              echo "$lines"
+                bin_path="../bin/simulation/${balance}/PARSIR-simulator-${config}"
+                mv ../bin/PARSIR-simulator-${model}-${q}${NUMA}${BALANCE} ${bin_path}
+                echo "$lines"
+                echo "Simulation for $config"
+                ${bin_path} | grep 'Barrier measures\|throughput' >../bin/simulation/logs/${run}_${config}.log
+                echo "$lines"
+                echo "Log file created at: ../bin/simulation/logs/${run}_${config}.log"
+                echo "$lines"
+              done
             done
           done
         done
@@ -65,10 +70,11 @@ for model in "${models[@]}"; do
   done
 done
 
-
 unset lookaheads
 unset alphas
 unset objects
 unset queue
 unset numa
 unset models
+unset balanced
+unset runs
